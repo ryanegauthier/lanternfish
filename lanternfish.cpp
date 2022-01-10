c3e78c9cd42032dc67e53492064ed890763c78dc
Ryan Gauthier <ryan.eugene.gauthier@gmail.com>
2022-01-09 18:00:16 -0800

    Update lanternfish.cpp
    removed DEBUG code reformatted functions and overall code

--- lanternfish.cpp
+++ lanternfish.cpp
@@ -5,48 +5,45 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <chrono>
+#include <ctime>  
 using namespace std;
 
-vector<int> resettimer (vector<int> arg) {
-  for (int n=0; n<arg.size(); ++n)
-    if(arg[n] < 0)
-      arg[n] = 6;
-  return arg;
+void advance_one_day (vector<int> &arg);
+
+void generatenewlanternfish (vector<int> &arg);
+
+vector<int> inputFile (string fileName);
+
+int main () {
+
+  vector<int> lanternfish = inputFile("example.txt");
+  
+  int days_to_evaluate;
+  cout << "How many days to observe? ";
+  cin >> days_to_evaluate;
+
+  cout << "Initial state: ";
+  for (int i = 0; i < lanternfish.size(); i++)
+      cout << lanternfish[i] << " ";
+  cout << "\n";
+  
+  std::chrono::duration<double> elapsed_seconds;
+  for (int i=0; i<days_to_evaluate; ++i) {
+    sort(lanternfish.begin(), lanternfish.end());      
+    advance_one_day(lanternfish);
+    generatenewlanternfish(lanternfish);
+  }
+  cout << "After " + to_string(days_to_evaluate) + " days there are " + to_string(lanternfish.size()) + " lanternfish.";
+  cout << "\n";
+
+  return 0;
 }
 
-vector<int> generatenewlanternfish (vector<int> arg) {
-  for (int n=0; n<arg.size(); n++)
-    if(arg[n] < 0)
-      arg.push_back(8);
-
-  arg = resettimer(arg);
-  return arg;
-}
-
-vector<int> advance_one_day (vector<int> arg) {
-  int initialcount = arg.size();
-
-  for (int n=0; n < initialcount; n++)
-    arg[n]--;
-    for (int n=0; n < initialcount; n++) {
-      if(arg[n] >= 0)
-        break;
-      arg[n] = 6;
-      arg.push_back(8);
-    }
- 
-//   arg = generatenewlanternfish(arg);
-  sort(arg.begin(), arg.end());  
-
-  return arg;
-}
-
-
-int main ()
-{
+vector<int> inputFile (string fileName){
   string line;
   string csv_values;
-  ifstream myfile ("example.txt");
+  ifstream myfile (fileName);
   if (myfile.is_open())
   {
     while ( getline (myfile,line) )
@@ -58,37 +55,28 @@ int main ()
 
   else cout << "Unable to open file"; 
 
-  vector<int> lanternfish;
+  vector<int> initialState;
   stringstream s_stream(csv_values); //create string stream from the string
   while(s_stream.good()) {
       string substr;
       getline(s_stream, substr, ','); //get first string delimited by comma
-      lanternfish.push_back(stoi(substr));
+      initialState.push_back(stoi(substr));
   }
-    
-    int days_to_evaluate;
-    cout << "How many days to observe? ";
-    cin >> days_to_evaluate;
+  return initialState;
+}
 
-    cout << "Initial state: ";
-    for (int i = 0; i < lanternfish.size(); i++)
-        cout << lanternfish[i] << " ";
-    cout << "\n";
-
-    for (int i=0; i<days_to_evaluate; ++i) {
-      lanternfish = advance_one_day(lanternfish);
-    //   cout << "After " + to_string(i+1) + " day(s): ";
-
-    //   for (int i = 0; i < lanternfish.size(); i++)
-    //     cout << lanternfish[i] << " ";
-    //   cout << "\n";
+void generatenewlanternfish (vector<int> &arg) {
+  for (int n=0; n<arg.size(); n++){
+    if(arg[n] < 0) {
+      arg[n] = 6;
+      arg.push_back(8);
+    } else {
+      break;
     }
+  }
+}
 
-    //   for (int i = 0; i < lanternfish.size(); i++)
-    //     cout << lanternfish[i] << " ";
-    //   cout << "\n";
-      cout << "After " + to_string(days_to_evaluate) + " days there are " + to_string(lanternfish.size()) + " lanternfish.";
-      cout << "\n";
-
-  return 0;
+void advance_one_day (vector<int> &arg) {
+  for (int n=0; n < arg.size(); n++)
+    arg[n]--;
 }
